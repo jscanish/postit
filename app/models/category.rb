5f70f47cd0f5b32d01c616c47dd5d6a1599e3eae
Josh <jscanish@gmail.com>
2013-07-20 22:52:26 -0400

    changing categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   has_many :postcategories
   has_many :posts, through: :postcategories
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
