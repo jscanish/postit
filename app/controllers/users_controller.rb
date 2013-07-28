1b82d4d6440c0305057c9c7783abc739ae1c1056
Josh <jscanish@gmail.com>
2013-07-28 03:36:55 -0400

    automatic sign in at registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path, notice: "You're registered! Now go log in!"
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "You're registered!"
     else
       render :new
     end
