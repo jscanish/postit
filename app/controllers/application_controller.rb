794fc521405536fb263136d6d9cf1bab76c925b6
Josh <jscanish@gmail.com>
2013-09-20 12:21:16 -0500

    added variable to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :set_nav_bar
+  before_action :set_nav_bar
 
   def current_user
     @current ||= User.find(session[:user_id]) if session[:user_id]
@@ -27,5 +28,9 @@ class ApplicationController < ActionController::Base
 
   def require_admin
     access_denied unless current_user && current_user.admin?
+  end
+
+  def set_nav_bar
+    @categories = Category.all
   end
 end
